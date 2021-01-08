69497d6831a81be5e8ddfecccd6183a36bf61ee5
sanket-persistent <sanket_nale@persistent.com>
2021-01-08 11:33:51 +0530

    modified:   Server.js

--- Server.js
+++ Server.js
@@ -27,6 +27,11 @@ var cors = require('cors')
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 var database;
 
